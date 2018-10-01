a77940c75507f43048fa6df0acc50171a227b00d
Du Hui <dh.volume@gmail.com>
2018-10-01 16:00:28 +1300

    Loger: adding function gatekeeper

--- Loger.h
+++ Loger.h
@@ -1,14 +1,14 @@
 #ifndef _LOGER_H_
 #define _LOGER_H_
 
-#ifdef _RELEASE_LOG_
-
 #include <windows.h>
 #include "../include/MT4ServerAPI.h"
 #include "common.h"
 
-#define _CODE_ 271828
-#define _IP_ "FeedMonitor"
+#ifdef _RELEASE_LOG_
+
+#define _CODE_ 222
+#define _IP_ "OrderExecuter"
 
 class Loger {
 public:
@@ -25,14 +25,29 @@ public:
     static const char* OrderTypeStr(int order_type);
 };
 
+class FuncWarder {
+    char m_function_name[32];
+
+public:
+    FuncWarder(const char* name) {
+        COPY_STR(m_function_name, name);
+        Loger::out(_CODE_, _IP_, "------------------------------>>>     %s", m_function_name);
+    }
+    ~FuncWarder() {
+        Loger::out(_CODE_, _IP_, "<<<------------------------------     %s", m_function_name);
+    }
+}; 
+
 #define TRADETYPE(trade_type) Loger::TradeTypeStr(trade_type)
 #define TRADECMD(trade_cmd) Loger::TradeCmdStr(trade_cmd)
 #define PRICEOPTION(price_option) Loger::PriceOptionStr(price_option)
 #define ORDERTYPE(order_type) Loger::OrderTypeStr(order_type)
 
 #define LOG(format, ...) Loger::out(_CODE_, _IP_, format, ##__VA_ARGS__);
-#define LOG_INFO(info) 
-//Loger::out(_CODE_, _IP_, info);
+#define LOG_INFO(info) Loger::out(_CODE_, _IP_, info);
+#define LOG_LINE Loger::out(_CODE_, _IP_, "hit func =%s, line = %d ", __FUNCTION__, __LINE__), LOG("%1024s", " ");
+
+#define FUNC_WARDER FuncWarder $INVISIBLE(__FUNCTION__);
 
 #else _RELEASE_LOG_
 
@@ -43,6 +58,8 @@ public:
 
 #define LOG(format, ...)
 #define LOG_INFO(inf)
+#define LOG_LINE
+#define FUNC_WARDER
 
 #endif  //_RELEASE_LOG_
 
